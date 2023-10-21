bdb4c201caa3dfbd60228c1f4eda97bb0ae0576e
Berkayszk <testmail>
2023-10-21 20:17:35 +0300

    Initial Commit

---
+++ FoodyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodyApp
+//
+//  Created by Berkay Sazak on 21.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
